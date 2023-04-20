4e14f3c88e6718da76908c5cd53be8d5a9938308
NoahKKA <noahhhhh.a@gmail.com>
2023-04-19 20:53:55 -0700

    Adding styles to REST-Rant

--- controllers/places.js
+++ controllers/places.js
@@ -3,12 +3,14 @@ const router = require('express').Router()
 // More code here in a moment
 router.get('/', (req, res) => {
     let places = [{
+        id: 0,
         name: 'H-Thai-ML',
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
         pic: 'http://placekitten.com/250/250'
       }, {
+        id: 1,
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -4,7 +4,7 @@ const Def = require('../deafult')
 function indexPlaces(data){
     let placeFormatted = data.places.map((place) => {
         return (
-            <div>
+            <div key={place.id}>
                 <h2>{place.name}</h2>
                 <img src={place.pic} alt={place.name}/>
             </div>
